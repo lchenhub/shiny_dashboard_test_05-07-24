bc8c0732248e7228d4057f96275d1e856c2c41a0
hazelvaq <hvaquero98@gmail.com>
2024-04-16 11:43:00 -0700

    text for about section

--- ui.R
+++ ui.R
@@ -14,7 +14,7 @@ sidebar <- dashboardSidebar(
     menuItem(text = h4("Welcome"), tabName = "welcome", icon = icon("star")),
     menuItem(text = h4("Index"), tabName = "index", icon = icon("map")),
     menuItem(text = h4(HTML("Explore Your Hazards")), tabName = "hazards", icon = icon("school")),
-    menuItem(text = h4("Adaptation"), tabName = "guide", icon = icon("person")),
+    menuItem(text = h4("Socioeconomic"), tabName = "socio", icon = icon("person")),
     menuItem(text = h4("User guide"), tabName = "guide", icon = icon("users"))
     
   )#END sidebarMENU
@@ -22,113 +22,67 @@ sidebar <- dashboardSidebar(
   
 )#END dashboard Sidebar
   
-# -------- leaflet map-----------------------------
-
 
 # -------- dashboard Body-----------------------------
 body <- dashboardBody(
-  
-  #tabItems ----
   tabItems(
-    
     tabItem(tabName = "welcome",
-            
-            # left hand column
-            column(width = 12,
-                   
-                   # background info box
-                   box(width = NULL,
-                       
-                       title = tags$strong("Welcome"),
-                       
-                       "CASAschool Interactive Dashboard.",
-                       
-                       
-                   ),# END background info box
-                   #leaflet map ---
-                   leafletOutput("map")
-            ) #END left-hand column
-    ),#END welcome tabItem
-    
-    #Index tabItem ----
-    tabItem(tabName = "index"
-            
-            
-            ## Add index map
-            
-            
-    ),#END of Index tabItem
-    
-    # Dashboard tabItem ----
+      column(width = 12,
+        box(width = NULL,
+          title = h3(tags$strong("Welcome!")),
+          #includeMarkdown("text/about.md")
+          "Filler"
+        ),
+        leafletOutput("map")
+      )
+    ),
+    tabItem(tabName = "index",
+      # Content for the index tab
+    ),
     tabItem(tabName = "hazards",
-            
-            # Fluidrow ----
-            fluidRow(
-              # input box
-              box(
-                width = 12,
-                
-                title = h2(tags$strong("Santa Paula Unified School district")),
-                
-                  tabsetPanel(
-                    tabPanel(h4("Extreme Heat"),
-                             
-                             # Extreme Heat Plot
-                             fluidRow(
-                               tags$style(".nav-tabs-custom {box-shadow:none;}
-                                          "),
-                               
-                               # extreme heat plot ---
-                               box(width = 6,
-                                   style = "border: none; border-width:0,",
-                                   plotOutput('hazard_plot')
-                                   
-                               ), #END extreme heat plot
-                               
-                               # Text on the right of graph description ---
-                               column(width = 6,
-                                      
-                                      box(
-                                        width = NULL,
-                                        style = "border: none; border-width:0;",
-                                        h3("What does this graph tell us?"),
-                                        p("The Santa Paula Unified School District
-                                          will see an increase in extreme heat days 
-                                          in the upcoming years."),
-                                        h3("How is extreme heat measured?"),
-                                        p("Extreme heat is measured as days 
-                                          where the daily maximum temperature is
-                                          above the 98th percentile of observed 
-                                          historical temperatures"),
-                                        h3("Source:")
-                                      )
-                                      
-                                      )
-                             )
-                             
-                             ),
-                    tabPanel(h4("Extreme Precipitation")),
-                    tabPanel(h4("Wildfire")),
-                    tabPanel(h4("Flooding")),
-                    tabPanel(h4("Sea Level Rise"))
-                    
-                    
-                  ),#END of tabsetPanel
-                
-                
-                
-                
-                
-              )#End input box
-              
-            )#END Fluidrow
-            
-    
-  )#END of tabItems
-  
-  
-) #END dashboard Body
+      fluidRow(
+        box(
+          width = 12,
+          title = h2(tags$strong("Santa Paula Unified School district")),
+          tabsetPanel(
+            tabPanel(h4("Extreme Heat"),
+              fluidRow(
+                tags$style(".nav-tabs-custom {box-shadow:none;}"),
+                box(width = 6,
+                  style = "border: none; border-width:0;",
+                  plotOutput('hazard_plot')
+                ),
+                column(width = 6,
+                  box(
+                    width = NULL,
+                    style = "border: none; border-width:0;",
+                    h3("What does this graph tell us?"),
+                    p("The Santa Paula Unified School District will see an increase in extreme heat days in the upcoming years."),
+                    h3("How is extreme heat measured?"),
+                    p("Extreme heat is measured as days where the daily maximum temperature is above the 98th percentile of observed historical temperatures"),
+                    h3("Source:")
+                  )
+                )
+              )
+            ),
+            tabPanel(h4("Extreme Precipitation")),
+            tabPanel(h4("Wildfire")),
+            tabPanel(h4("Flooding")),
+            tabPanel(h4("Sea Level Rise"))
+          )
+        )
+      )
+    ),
+    tabItem(tabName = "socio",
+      fluidRow(
+        box(width = 8,
+          # Content for the socioeconomic tab
+        )
+      )
+    )
+  )
 )
+
 
 # -------- Combine all in dashboardPage----------------
 dashboardPage(header, sidebar, body,skin = "yellow")
